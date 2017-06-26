4f5ef50261c704d3f48af6f409319bfa5e0b428d
Archie L. Cobbs <archie.cobbs@gmail.com>
2017-06-26 12:37:17 -0500

    Add @GridColumn annotation and GridColumnScanner.

---
+++ dellroad-stuff-vaadin8/src/main/java/org/dellroad/stuff/vaadin8/GridColumn.java
@@ -0,0 +1,233 @@
+
+/*
+ * Copyright (C) 2011 Archie L. Cobbs. All rights reserved.
+ */
+
+package org.dellroad.stuff.vaadin8;
+
+import com.vaadin.server.SerializableComparator;
+import com.vaadin.ui.StyleGenerator;
+import com.vaadin.ui.components.grid.DescriptionGenerator;
+import com.vaadin.ui.components.grid.SortOrderProvider;
+import com.vaadin.ui.renderers.Renderer;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Provides the information necessary to auto-generate a {@link Grid.Column} based on the annotated getter method.
+ *
+ * <p>
+ * Example:
+ * <blockquote><pre>
+ * // Container backing object class
+ * public class User {
+ *
+ *     private String username;
+ *     private LocalDate dateOfBirth;
+ *
+ *     <b>&#64;GridColumn(caption = "User name", expandRatio = 3)</b>
+ *     public String getUsername() {
+ *         return this.username;
+ *     }
+ *     public void setUsername(String username) {
+ *         this.username = username;
+ *     }
+ *
+ *     <b>&#64;GridColumn(caption = "Birthday", sortable = true)</b>
+ *     public LocalDate getDateOfBirth() {
+ *         return this.dateOfBirth;
+ *     }
+ *     public void setDateOfBirth(LocalDate dateOfBirth) {
+ *         this.dateOfBirth = dateOfBirth;
+ *     }
+ * }
+ *
+ * // Build Grid with auto-generated columns
+ * Grid&lt;User&gt; grid = new GridColumnScanner(User.class).buildGrid();
+ * ...
+ * </pre></blockquote>
+ *
+ * <p>
+ * Some details regarding {@link GridColumn &#64;GridColumn} annotations:
+ *  <ul>
+ *  <li>Only non-void methods taking zero parameters are supported; {@link GridColumn &#64;GridColumn}
+ *      annotations on other methods are ignored</li>
+ *  <li>Protected, package private, and private methods are supported.</li>
+ *  <li>{@link GridColumn &#64;GridColumn} annotations declared in super-types (including interfaces) are supported</li>
+ *  <li>If a method and the superclass or superinterface method it overrides are both annotated with
+ *      {@link GridColumn &#64;GridColumn}, then the overridding method's annotation takes precedence.
+ *  <li>If two methods with different names are annotated with {@link GridColumn &#64;GridColumn} for the same
+ *      {@linkplain #value property name}, then the declaration in the class which is a sub-type of the other
+ *      wins (if the two classes are equal or not comparable, an exception is thrown). This allows subclasses
+ *      to "override" which method supplies a given property.</li>
+ *  </ul>
+ *
+ * @see GridColumnScanner
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@Documented
+public @interface GridColumn {
+
+    /**
+     * Get the column ID.
+     *
+     * <p>
+     * If this is left unset (empty string), then the bean property name of the annotated bean property "getter" method is used.
+     *
+     * @return property name
+     */
+    String value() default "";
+
+    /**
+     * Get the caption for the Grid column.
+     *
+     * @return column caption
+     * @see Grid.Column#setCaption
+     */
+    String caption() default "";
+
+    /**
+     * Get the in-memory comparator for the Grid column.
+     *
+     * @return column comparator
+     * @see Grid.Column#setComparator
+     */
+    @SuppressWarnings("rawtypes")
+    Class<? extends SerializableComparator> comparator() default SerializableComparator.class;
+
+    /**
+     * Get the description generator for the Grid column.
+     *
+     * @return column description generator
+     * @see Grid.Column#setDescriptionGenerator
+     */
+    @SuppressWarnings("rawtypes")
+    Class<? extends DescriptionGenerator> descriptionGenerator() default DescriptionGenerator.class;
+
+    /**
+     * Get whether the Grid column is editable.
+     *
+     * @return whether editable
+     * @see Grid.Column#setEditable
+     */
+    boolean editable() default false;
+
+    /**
+     * Get the expand ratio for the Grid column.
+     *
+     * @return column expand ratio
+     * @see Grid.Column#setExpandRatio
+     */
+    int expandRatio() default -1;
+
+    /**
+     * Get whether the Grid column is hidable.
+     *
+     * @return whether hidable
+     * @see Grid.Column#setHidable
+     */
+    boolean hidable() default false;
+
+    /**
+     * Get whether the Grid column is hidden.
+     *
+     * @return whether hidden
+     * @see Grid.Column#setHidden
+     */
+    boolean hidden() default false;
+
+    /**
+     * Get the hiding toggle caption for the Grid column.
+     *
+     * @return column hiding toggle caption
+     * @see Grid.Column#setHidingToggleCaption
+     */
+    String hidingToggleCaption() default "";
+
+    /**
+     * Get the debug ID for the Grid column.
+     *
+     * @return column debug ID
+     * @see Grid.Column#setId
+     */
+    String id() default "";
+
+    /**
+     * Get the maximum width for the Grid column.
+     *
+     * @return column maximum width
+     * @see Grid.Column#setMaximumWidth
+     */
+    double maximumWidth() default Float.NaN;
+
+    /**
+     * Get the minimum width for the Grid column.
+     *
+     * @return column minimum width
+     * @see Grid.Column#setMinimumWidth
+     */
+    double minimumWidth() default Float.NaN;
+
+    /**
+     * Get the renderer for the Grid column.
+     *
+     * @return column renderer
+     * @see Grid.Column#setRenderer
+     */
+    @SuppressWarnings("rawtypes")
+    Class<? extends Renderer> renderer() default Renderer.class;
+
+    /**
+     * Get whether the Grid column is resizable.
+     *
+     * @return whether resizable
+     * @see Grid.Column#setResizable
+     */
+    boolean resizable() default false;
+
+    /**
+     * Get whether the Grid column is sortable.
+     *
+     * @return whether sortable
+     * @see Grid.Column#setSortable
+     */
+    boolean sortable() default false;
+
+    /**
+     * Get the sort order provider for the Grid column.
+     *
+     * @return column sort order provider
+     * @see Grid.Column#setSortOrderProvider
+     */
+    Class<? extends SortOrderProvider> sortOrderProvider() default SortOrderProvider.class;
+
+    /**
+     * Get the back-end sort properties for the Grid column.
+     *
+     * @return column back-end sort properties
+     * @see Grid.Column#setSortOrderProvider
+     */
+    String[] sortProperty() default {};
+
+    /**
+     * Get the style generator for the Grid column.
+     *
+     * @return column style generator
+     * @see Grid.Column#setStyleGenerator
+     */
+    @SuppressWarnings("rawtypes")
+    Class<? extends StyleGenerator> styleGenerator() default StyleGenerator.class;
+
+    /**
+     * Get the width for the Grid column.
+     *
+     * @return column width
+     * @see Grid.Column#setWidth
+     */
+    double width() default Float.NaN;
+}
---
+++ dellroad-stuff-vaadin8/src/main/java/org/dellroad/stuff/vaadin8/GridColumnScanner.java
@@ -0,0 +1,155 @@
+
+/*
+ * Copyright (C) 2011 Archie L. Cobbs. All rights reserved.
+ */
+
+package org.dellroad.stuff.vaadin8;
+
+import com.vaadin.ui.Grid;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.dellroad.stuff.java.MethodAnnotationScanner;
+
+/**
+ * Scans a Java class hierarchy for {@link GridColumn &#64;GridColumn} annotated getter methods, and auto-generates and
+ * configures corresponding {@link Grid.Column}s on a {@link Grid} instance.
+ *
+ * <p>
+ * See {@link GridColumn} for an example of usage.
+ *
+ * @param <T> Java class to be introspected
+ * @see GridColumn &#64;GridColumn
+ */
+public class GridColumnScanner<T> {
+
+    private final Class<T> type;
+    private final HashMap<String, MethodAnnotationScanner<T, GridColumn>.MethodInfo> columnMap = new HashMap<>();
+
+    /**
+     * Constructor.
+     *
+     * @param type Java class to be introspected
+     * @throws IllegalArgumentException if {@code type} is null
+     * @throws IllegalArgumentException if an annotated method with no {@linkplain GridColumn#value property name specified}
+     *  has a name which cannot be interpreted as a bean property "getter" method
+     * @throws IllegalArgumentException if {@code type} has two {@link GridColumn &#64;GridColumn}-annotated
+     *  fields or methods with the same {@linkplain GridColumn#value property name}
+     */
+    public GridColumnScanner(Class<T> type) {
+
+        // Sanity check
+        if (type == null)
+            throw new IllegalArgumentException("null type");
+        this.type = type;
+
+        // Scan for @GridColumn annotations
+        final Set<MethodAnnotationScanner<T, GridColumn>.MethodInfo> gridColumnMethods
+          = new MethodAnnotationScanner<T, GridColumn>(this.type, GridColumn.class).findAnnotatedMethods();
+
+        // Check for duplicate @GridColumn names
+        final Comparator<Method> methodComparator = Comparator.comparing(Method::getDeclaringClass,
+          AnnotationUtil.getClassComparator(false));
+        for (MethodAnnotationScanner<T, GridColumn>.MethodInfo methodInfo : gridColumnMethods) {
+            final String propertyName = this.getPropertyName(methodInfo);
+
+            // Check for name conflict
+            final MethodAnnotationScanner<T, GridColumn>.MethodInfo previousInfo = this.columnMap.get(propertyName);
+            if (previousInfo == null) {
+                this.columnMap.put(propertyName, methodInfo);
+                continue;
+            }
+
+            // If there is a name conflict, the sub-type method declaration wins
+            switch (methodComparator.compare(previousInfo.getMethod(), methodInfo.getMethod())) {
+            case 0:
+                throw new IllegalArgumentException("duplicate @" + GridColumn.class.getSimpleName()
+                  + " declaration for property `" + propertyName + "' on method " + previousInfo.getMethod()
+                  + " and " + methodInfo.getMethod() + " declared in the same class");
+            case 1:
+                this.columnMap.put(propertyName, methodInfo);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    /**
+     * Build a {@link Grid} with columns auto-generated from introspected {@link GridColumn &#64;GridColumn} annotations.
+     *
+     * @return new {@link Grid}
+     */
+    public Grid<T> buildGrid() {
+
+        // Build property set
+        final SimplePropertySet<T> propertySet = new SimplePropertySet<>();
+        for (Map.Entry<String, MethodAnnotationScanner<T, GridColumn>.MethodInfo> e : this.columnMap.entrySet()) {
+            final String propertyName = e.getKey();
+            final MethodAnnotationScanner<T, GridColumn>.MethodInfo methodInfo = e.getValue();
+            final Method getter = methodInfo.getMethod();
+            final Method setter = methodInfo.getSetter();
+            propertySet.add(getter.getReturnType(), propertyName, methodInfo.getAnnotation().caption(), getter, setter);
+        }
+
+        // Create grid
+        final Grid<T> grid = Grid.withPropertySet(propertySet);
+
+        // Get default annotation
+        final GridColumn defaults = GridColumnScanner.getDefaults();
+
+        // Modify columns
+        for (Map.Entry<String, MethodAnnotationScanner<T, GridColumn>.MethodInfo> e : this.columnMap.entrySet()) {
+            final String propertyName = e.getKey();
+            final MethodAnnotationScanner<T, GridColumn>.MethodInfo methodInfo = e.getValue();
+
+            // Get column
+            final Grid.Column<T, ?> column = grid.getColumn(propertyName);
+
+            // Apply annotation values
+            AnnotationUtil.apply(column, methodInfo.getAnnotation(), defaults, (method, name) -> true);
+        }
+
+        // Done
+        return grid;
+    }
+
+    @GridColumn
+    private static GridColumn getDefaults() {
+        return AnnotationUtil.getAnnotation(GridColumn.class, GridColumnScanner.class, "getDefaults");
+    }
+
+    /**
+     * Get the property name from the annotation.
+     *
+     * @param methodInfo method info
+     */
+    protected String getPropertyName(MethodAnnotationScanner<T, GridColumn>.MethodInfo methodInfo) {
+        return methodInfo.getAnnotation().value().length() > 0 ?
+          methodInfo.getAnnotation().value() : methodInfo.getMethodPropertyName();
+    }
+
+    /**
+     * Get the type associated with this instance.
+     *
+     * @return backing object type
+     */
+    public Class<T> getType() {
+        return this.type;
+    }
+
+    /**
+     * Get the annotations found through introspection keyed by property name.
+     *
+     * @return columns keyed by property name
+     */
+    public Map<String, MethodAnnotationScanner<T, GridColumn>.MethodInfo> getColumnMap() {
+        return Collections.unmodifiableMap(this.columnMap);
+    }
+}
+
