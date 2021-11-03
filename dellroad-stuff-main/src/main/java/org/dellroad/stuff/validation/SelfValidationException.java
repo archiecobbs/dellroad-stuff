944659d9d7901861b1427cf7d6b465b5a3cf54a7
Archie L. Cobbs <archie.cobbs@gmail.com>
2021-11-03 13:54:49 -0500

    Clarify that SelfValidationException constructor takes a template.

--- dellroad-stuff-main/src/main/java/org/dellroad/stuff/validation/SelfValidationException.java
+++ dellroad-stuff-main/src/main/java/org/dellroad/stuff/validation/SelfValidationException.java
@@ -12,24 +12,69 @@ package org.dellroad.stuff.validation;
  * Instances will be automatically caught and converted into a constraint violation using the exception's message
  * if any, otherwise the default message.
  *
+ * <p>
+ * Note that the {@code message} constructor parameter is a JSR 303 template string; to provide a plain text string,
+ * use {@link #escapeTemplate SelfValidationException.escapeTemplate()} or {@link #plain SelfValidationException.plain()}.
+ *
  * @see SelfValidating
  */
 @SuppressWarnings("serial")
 public class SelfValidationException extends Exception {
 
+    /**
+     * Constructor.
+     */
     public SelfValidationException() {
     }
 
+    /**
+     * Constructor.
+     *
+     * @param message validation error message template
+     */
     public SelfValidationException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor.
+     *
+     * @param message validation error message template
+     */
     public SelfValidationException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructor.
+     *
+     * @param message validation error message template
+     */
     public SelfValidationException(String message, Throwable cause) {
         super(message, cause);
     }
-}
 
+    /**
+     * Build a {@link SelfValidationException} with the given plain text message.
+     *
+     * @param message plain message string, or null for none
+     * @throws SelfValidationException always
+     * @see #escapeTemplate SelfValidationException.escapeTemplate()
+     */
+    public static SelfValidationException plain(String message) {
+        return new SelfValidationException(message != null ? SelfValidationException.escapeTemplate(message) : null);
+    }
+
+    /**
+     * Escape any special characters in the given literal string, returning an equivalent JSR 303 message template.
+     *
+     * @param message plain message string
+     * @return JSR 303 message template
+     * @throws IllegalArgumentException if {@code message} is null
+     */
+    public static String escapeTemplate(String message) {
+        if (message == null)
+            throw new IllegalArgumentException("null message");
+        return message.replaceAll("([\\\\{}$])", "\\\\$1");
+    }
+}
