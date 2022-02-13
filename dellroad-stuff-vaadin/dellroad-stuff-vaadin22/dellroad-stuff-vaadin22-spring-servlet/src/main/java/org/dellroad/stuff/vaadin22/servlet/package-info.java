5ad30790a96c8538579087f23bff1df423395806
Archie L. Cobbs <archie.cobbs@gmail.com>
2022-02-13 16:08:09 -0600

    Add package Javadoc description line.

--- dellroad-stuff-vaadin/dellroad-stuff-vaadin22/dellroad-stuff-vaadin22-spring-servlet/src/main/java/org/dellroad/stuff/vaadin22/servlet/package-info.java
+++ dellroad-stuff-vaadin/dellroad-stuff-vaadin22/dellroad-stuff-vaadin22-spring-servlet/src/main/java/org/dellroad/stuff/vaadin22/servlet/package-info.java
@@ -3,4 +3,7 @@
  * Copyright (C) 2022 Archie L. Cobbs. All rights reserved.
  */
 
+/**
+ * {@link org.dellroad.stuff.vaadin22.servlet.SimpleSpringServlet} and related classes.
+ */
 package org.dellroad.stuff.vaadin22.servlet;
